fbbaf2364fc0c0f7634ee24f859d1dc720e48d19
Ahmad Imran <ahmadcancerian3@gmail.com>
2025-10-27 22:07:03 +0500

    resolve deployment error5

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,27 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
-
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "*"
-        : "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -31,18 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  // Serve frontend build
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // Catch-all route to serve React index.html
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-
-
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
